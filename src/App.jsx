aa69eca492ea591b2a4e1129063e6edb33171851
Nisarga-MS <84907841+Nisarga-MS@users.noreply.github.com>
2024-01-24 18:28:31 +0530

    Added posterchunk rightpart

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import PosterChunk from './components/PosterChunk/PosterChunk';
 
 function App() {
   return (
     <div>
-      App   
+      <PosterChunk />  
     </div>
   );
 }
---
+++ src/components/PosterChunk/PosterChunk.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from "./PosterChunk.module.css"
+import Poster from "../../assets/images/poster.png"
+import Lock from "../../assets/icons/lock.svg";
+
+export default function PosterChunk() {
+  return (
+    <div className={styles.poster}>
+        <div className={styles.focusParts}>
+            <img src={Poster} alt="poster" />
+            <h1>Pocket Notes</h1>
+            <p>Send and receive messages without keeping your phone online.
+            Use Pocket Notes on up to 4 linked devices and 1 mobile phone</p>
+        </div>
+        <div className={styles.endLine}>
+            <span><img src={Lock} alt="lock"/> end-to-end encrypted</span>
+        </div>
+    </div>
+  )
+}
